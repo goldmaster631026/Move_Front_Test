431a54a60a257ac6ea884d5e23b04948d86d1b52
goldmaster631026 <goldmaster631026@gmail.com>
2024-09-17 13:21:19 +1000

    Erwin

--- src/pages/movies/Add.tsx
+++ src/pages/movies/Add.tsx
@@ -32,7 +32,22 @@ const AddMovie = () => {
     //       }
     //     }
     //   })
-    
+
+    // const userInfo = createSlice({
+    //     name: "auth",
+    //     initialState,
+    //     reducers: {
+    //       logIn(state, action) {
+    //         state.user = action.payload;
+    //         state.isLoggedIn = true;
+    //         localStorage.setItem("isLoggedIn", "true");
+    //       },
+    //       hasError(state, action) {
+    //         state.error = action.payload
+    //       }
+    //     }
+    //   })
+
     const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const videoTitle = e.target.value;
         setVideoData({
